860e3f19d75e85725c8d52cee3611bcbd524b547
jeremiaranggasetyawan <jeremiaranggas@gmail.com>
2024-09-13 21:41:02 +0700

    fix pws again

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -122,4 +122,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","jeremia-rangga-mentalhealthtracker.pbp.cs.ui.ac.id"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://jeremia-rangga-mentalhealthtracker.pbp.cs.ui.ac.id"]
