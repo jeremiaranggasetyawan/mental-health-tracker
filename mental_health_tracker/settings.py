12f5e86d82622be7a8fe6653dad33553abfd155a
jeremiaranggasetyawan <jeremiaranggas@gmail.com>
2024-09-15 18:54:12 +0700

    fix pws ke duaratus kalinya

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-k*rn%qbg+5pctzux&oh50)607l5n!_z#yxeg79#_@sa&9tnv$)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "http://jeremia-rangga-mentalhealthtracker.pbp.cs.ui.ac.id"]
+ALLOWED_HOSTS = ["localhost", "jeremia-rangga-mentalhealthtracker.pbp.cs.ui.ac.id"]
 
 # Application definition
 
